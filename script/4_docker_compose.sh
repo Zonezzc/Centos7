e7fbd574abb62abbccb5be2cf70f13a135b7d12b
zonezzc <zonezzc@foxmail.com>
2023-10-17 23:21:11 +0800

    增加是否增加代理判断

--- script/4_docker_compose.sh
+++ script/4_docker_compose.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# 检测当前IP地址是否在中国
+ip_result=$(curl -s cip.cc)
+if [[ $ip_result == *"中国"* ]]; then
+    ghproxy_url="https://ghproxy.com/"
+else
+    ghproxy_url=""
+fi
+
 # 下载docker-compose
 sudo curl -L "${ghproxy_url}https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
