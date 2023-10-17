199c1683a2c69089d49695da9eed6cf4fecd6d9f
zonezzc <zonezzc@foxmail.com>
2023-10-17 23:40:59 +0800

    更该docker-compose下载方式

--- script/4_docker_compose.sh
+++ script/4_docker_compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 下载docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/2.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://zonezzc.5t-cdn.com/files/652eaadc916bc/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 
 # 给予执行权限
 sudo chmod +x /usr/local/bin/docker-compose
