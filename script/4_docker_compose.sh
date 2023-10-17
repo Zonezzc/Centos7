837f318285b5d1d4f1b5b38a11b8530a6e9eb7fd
zonezzc <zonezzc@foxmail.com>
2023-10-17 23:24:49 +0800

    更新版本

--- script/4_docker_compose.sh
+++ script/4_docker_compose.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# 检测当前IP地址是否在中国
-ip_result=$(curl -s cip.cc)
-if [[ $ip_result == *"中国"* ]]; then
-    ghproxy_url="https://ghproxy.com/"
-else
-    ghproxy_url=""
-fi
-
 # 下载docker-compose
-sudo curl -L "${ghproxy_url}https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/2.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # 给予执行权限
 sudo chmod +x /usr/local/bin/docker-compose
